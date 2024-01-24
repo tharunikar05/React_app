5bbd9a616f3b09c0ef9b99a98838f075d2ed08c7
tharunikar05 <tharunikar.21ece@kongu.edu>
2024-01-24 10:15:24 +0530

    reactapp_props

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-import TestComp from './components/functionalcomponents/testComponents';
-import ReiteratorComp from './components/classcomponents/testclasscomponent';
+import PropsComponent from './components/functionalcomponents/PropsComponent';
 
 function App() {
   return (
     <div className="App">
-      <ReiteratorComp/>
+     <h1> <PropsComponent name="HELLO EVERYONE" course="MERN"></PropsComponent></h1>
       <header className="App-header">
-        <TestComp/>
+       
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
---
+++ src/components/functionalcomponents/PropsComponent.jsx
@@ -0,0 +1,11 @@
+function PropsComponent(props)
+{
+  return(
+    <div>
+       <h1> {props.name} <br></br>This is {props.course} class</h1>
+    </div>
+  )
+}
+export default PropsComponent;
+//http://localhost:3000/
+//files:/users/website namer
